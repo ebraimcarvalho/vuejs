10a4b40bf7a41f555d2022ace7dc5898165c72d9
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2021-01-18 23:03:25 -0300

    run vue cli in new local machine

--- vue-coder/vue-cli/projeto-simples/src/components/HelloWorld.vue
+++ vue-coder/vue-cli/projeto-simples/src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <input type="text" v-model="name">
+    <p>Name: {{name}}</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +37,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+      name: '',
+    }
   }
 }
 </script>
