39d909d8e2000d200c5a7a136938b8603baa9149
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2021-01-22 22:15:16 -0300

    activated and deactivated

--- vue-coder/vue-cli/citacoes/src/components/Citacoes.vue
+++ vue-coder/vue-cli/citacoes/src/components/Citacoes.vue
@@ -1,51 +1,70 @@
 <template>
-    <div class="citacoes">
-        <span>
-            <button @click="numero--">&lt;</button>
-            <button @click="numero++">&gt;</button>
-        </span>
-        <Citacao>
-            <h2 slot="fonte">{{citacoes[indice].fonte}}</h2>
-            <p slot="texto">{{citacoes[indice].texto}}</p>
-            <h6 slot="autor"><em>{{citacoes[indice].autor}}</em></h6>
-        </Citacao>
-    </div>
+  <div class="citacoes">
+    <span>
+      <button @click="numero--">&lt;</button>
+      <button @click="numero++">&gt;</button>
+    </span>
+    <Citacao>
+      <h2 slot="fonte">{{ citacoes[indice].fonte }}</h2>
+      <p slot="texto">{{ citacoes[indice].texto }}</p>
+      <h6 slot="autor">
+        <em>{{ citacoes[indice].autor }}</em>
+      </h6>
+    </Citacao>
+  </div>
 </template>
 
 <script>
-import Citacao from './Citacao.vue'
+import Citacao from "./Citacao.vue";
 export default {
-    components: { Citacao },
-    data() {
-        return {
-            numero: 0,
-            citacoes: [{
-                fonte: 'Jornal do Empreendedor',
-                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
-                autor: 'Margaret Mead'
-            }, { 
-                fonte: 'Frases de Mãe',
-                texto: 'Isso não é um quarto, é um chiqueiro.',
-                autor: 'Roberta'
-            }, {
-                fonte: 'Frases de Pai',
-                texto: 'Vou contar até 3! 1, 2, 2...',
-                autor: 'Gustavo'
-            }]
-        }
+  components: { Citacao },
+  data() {
+    return {
+      numero: 0,
+      citacoes: [
+        {
+          fonte: "Jornal do Empreendedor",
+          texto:
+            "Lembre-se sempre que você é absolutamente único. Assim como todos os outros.",
+          autor: "Margaret Mead",
+        },
+        {
+          fonte: "Frases de Mãe",
+          texto: "Isso não é um quarto, é um chiqueiro.",
+          autor: "Roberta",
+        },
+        {
+          fonte: "Frases de Pai",
+          texto: "Vou contar até 3! 1, 2, 2...",
+          autor: "Gustavo",
+        },
+      ],
+    };
+  },
+  computed: {
+    indice() {
+      return Math.abs(this.numero % 3);
     },
-    computed: {
-        indice() {
-            return Math.abs(this.numero % 3)
-        }
-    }
-}
+  },
+  created() {
+    console.log('created')
+  },
+  destroyed() {
+    console.log('destroyed')
+  },
+  activated() {
+    console.log('activated')
+  },
+  deactivated() {
+    console.log('deactivated')
+  },
+};
 </script>
 
 <style scoped>
-    .citacoes {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+.citacoes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
