1f54cfd0a3546886f2ae317c8f8cfb67a6060f72
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2021-01-25 23:43:06 -0300

    init animation

--- vue-coder/vue-cli/animacoes/animacoes-exercicios/src/App.vue
+++ vue-coder/vue-cli/animacoes/animacoes-exercicios/src/App.vue
@@ -1,13 +1,29 @@
 <template>
 	<div id="app" class="container-fluid">
 		<h1>Animações</h1>
+		<hr>
+		<b-button 
+			class="mb-4"
+			variant="primary"
+			@click="show = !show"
+			size="lg">
+				Toggle Mesage
+		</b-button>
+		<transition name="fade">
+			<b-alert variant="info" v-if="show" show>{{ msg }}</b-alert>
+		</transition>
 	</div>
 </template>
 
 <script>
 
 export default {
-
+	data() {
+		return {
+			msg: 'Uma mensagem de informação para o usuário',
+			show: false
+		}
+	}
 }
 </script>
 
@@ -20,5 +36,13 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 2s;
 }
 </style>
