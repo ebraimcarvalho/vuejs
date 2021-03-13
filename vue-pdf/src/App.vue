9d4f9fce2ad5d40da5bc0954844e1e97d267ff69
Ebraim de França Carvalho Filho <ebraim.carvalho@meta.com.br>
2021-03-13 17:45:21 -0300

    init vue pdf

---
+++ vue-pdf/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>PDF Reader</h1>
+    <input @change="previewFiles" type="file" name="pdfFile" id="pdfFile">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    async previewFiles(e) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = function() {
+        const lines = reader.result.split('\n')
+        console.log(lines)
+      }
+      reader.readAsText(file)
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
